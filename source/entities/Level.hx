82d506ae347f71e13fef132fc8679a35547d27c7
Toby Alden <toby.alden@gmail.com>
2019-11-24 20:12:02 -0800

    Add procedural generation

--- source/entities/Level.hx
+++ source/entities/Level.hx
@@ -3,37 +3,103 @@ package entities;
 import haxepunk.*;
 import haxepunk.graphics.tile.*;
 import haxepunk.masks.*;
+import haxepunk.math.*;
 import openfl.Assets;
 
 class Level extends Entity
 {
     public static inline var TILE_SIZE = 4;
+    public static inline var MIN_LEVEL_WIDTH = 320;
+    public static inline var MIN_LEVEL_HEIGHT = 180;
+    public static inline var MIN_LEVEL_WIDTH_IN_TILES = 80;
+    public static inline var MIN_LEVEL_HEIGHT_IN_TILES = 45;
+    public static inline var NUMBER_OF_ROOMS = 1;
+    public static inline var NUMBER_OF_HALLWAYS = 1;
+    public static inline var NUMBER_OF_SHAFTS = 1;
 
-    private var walls:Grid;
+    public var walls(default, null):Grid;
+    public var pathUpWalls(default, null):Grid;
     private var tiles:Tilemap;
+    private var levelType:String;
 
-    public function new(levelName:String) {
-        super();
+    public function new(x:Int, y:Int, levelType:String) {
+        super(x, y);
+        this.levelType = levelType;
         type = "walls";
-
-        loadLevel(levelName);
-
-        tiles = new Tilemap(
-            'graphics/tiles.png',
-            walls.width, walls.height, walls.tileWidth, walls.tileHeight
-        );
-        tiles.loadFromString(walls.saveToString(',', '\n', '1', '0'));
-
+        if(levelType == "room") {
+            loadLevel('${
+                Std.int(Math.floor(Random.random * NUMBER_OF_ROOMS))
+            }');
+        }
+        else if(levelType == "hallway") {
+            loadLevel('${
+                Std.int(Math.floor(Random.random * NUMBER_OF_HALLWAYS))
+            }');
+        }
+        else {
+            // levelType == "shaft"
+            loadLevel('${
+                Std.int(Math.floor(Random.random * NUMBER_OF_SHAFTS))
+            }');
+        }
+        if(Random.random < 0.5) {
+            flipHorizontally(walls);
+            flipHorizontally(pathUpWalls);
+        }
+        updateGraphic();
         graphic = tiles;
         mask = walls;
     }
 
+
+    override public function update() {
+        super.update();
+    }
+
+    public function flipHorizontally(wallsToFlip:Grid) {
+        for(tileX in 0...Std.int(wallsToFlip.columns / 2)) {
+            for(tileY in 0...wallsToFlip.rows) {
+                var tempLeft:Null<Bool> = wallsToFlip.getTile(tileX, tileY);
+                // For some reason getTile() returns null instead of false!
+                if(tempLeft == null) {
+                    tempLeft = false;
+                }
+                var tempRight:Null<Bool> = wallsToFlip.getTile(
+                    wallsToFlip.columns - tileX - 1, tileY
+                );
+                if(tempRight == null) {
+                    tempRight = false;
+                }
+                wallsToFlip.setTile(tileX, tileY, tempRight);
+                wallsToFlip.setTile(
+                    wallsToFlip.columns - tileX - 1, tileY, tempLeft
+                );
+            }
+        }
+    }
+
+    public function addPathsUp() {
+        for(tileX in 0...walls.columns) {
+            for(tileY in 0...walls.rows) {
+                if(pathUpWalls.getTile(tileX, tileY)) {
+                    walls.setTile(tileX, tileY);
+                }
+            }
+        }
+    }
+
     private function loadLevel(levelName:String) {
-        var xml = Xml.parse(Assets.getText('levels/${levelName}.oel'));
+        // Load geometry
+        var xml = Xml.parse(Assets.getText(
+            'levels/${levelType}/${levelName}.oel'
+        ));
         var fastXml = new haxe.xml.Fast(xml.firstElement());
         var segmentWidth = Std.parseInt(fastXml.node.width.innerData);
         var segmentHeight = Std.parseInt(fastXml.node.height.innerData);
         walls = new Grid(segmentWidth, segmentHeight, TILE_SIZE, TILE_SIZE);
+        pathUpWalls = new Grid(
+            segmentWidth, segmentHeight, TILE_SIZE, TILE_SIZE
+        );
         for (r in fastXml.node.walls.nodes.rect) {
             walls.setRect(
                 Std.int(Std.parseInt(r.att.x) / TILE_SIZE),
@@ -42,10 +108,83 @@ class Level extends Entity
                 Std.int(Std.parseInt(r.att.h) / TILE_SIZE)
             );
         }
+        if(levelType == "room") {
+            for (r in fastXml.node.pathUpWalls.nodes.rect) {
+                pathUpWalls.setRect(
+                    Std.int(Std.parseInt(r.att.x) / TILE_SIZE),
+                    Std.int(Std.parseInt(r.att.y) / TILE_SIZE),
+                    Std.int(Std.parseInt(r.att.w) / TILE_SIZE),
+                    Std.int(Std.parseInt(r.att.h) / TILE_SIZE)
+                );
+            }
+        }
+
+        // Load optional geometry
+        if(fastXml.hasNode.optionalWalls) {
+            for (r in fastXml.node.optionalWalls.nodes.rect) {
+                if(Random.random < 0.5) {
+                    continue;
+                }
+                walls.setRect(
+                    Std.int(Std.parseInt(r.att.x) / TILE_SIZE),
+                    Std.int(Std.parseInt(r.att.y) / TILE_SIZE),
+                    Std.int(Std.parseInt(r.att.w) / TILE_SIZE),
+                    Std.int(Std.parseInt(r.att.h) / TILE_SIZE)
+                );
+            }
+        }
     }
 
-    override public function update() {
-        super.update();
+    public function fillLeft(offsetY:Int) {
+        for(tileY in 0...MIN_LEVEL_HEIGHT_IN_TILES) {
+            walls.setTile(0, tileY + offsetY * MIN_LEVEL_HEIGHT_IN_TILES);
+        }
+    }
+
+    public function fillRight(offsetY:Int) {
+        for(tileY in 0...MIN_LEVEL_HEIGHT_IN_TILES) {
+            walls.setTile(
+                walls.columns - 1,
+                tileY + offsetY * MIN_LEVEL_HEIGHT_IN_TILES
+            );
+        }
+    }
+
+    public function fillTop(offsetX:Int) {
+        for(tileX in 0...MIN_LEVEL_WIDTH_IN_TILES) {
+            walls.setTile(tileX + offsetX * MIN_LEVEL_WIDTH_IN_TILES, 0);
+            // Clear paths up if not needed
+            for(tileY in 0...walls.rows) {
+                pathUpWalls.clearTile(
+                    tileX + offsetX * MIN_LEVEL_WIDTH_IN_TILES, tileY
+                );
+            }
+        }
+    }
+
+    public function fillBottom(offsetX:Int) {
+        for(tileX in 0...MIN_LEVEL_WIDTH_IN_TILES) {
+            walls.setTile(
+                tileX + offsetX * MIN_LEVEL_WIDTH_IN_TILES,
+                walls.rows - 1
+            );
+        }
+    }
+
+    public function updateGraphic() {
+        tiles = new Tilemap(
+            'graphics/tiles.png',
+            walls.width, walls.height, walls.tileWidth, walls.tileHeight
+        );
+        tiles.loadFromString(walls.saveToString(',', '\n', '1', '0'));
+        for(tileX in 0...walls.columns) {
+            for(tileY in 0...walls.rows) {
+                if(pathUpWalls.getTile(tileX, tileY)) {
+                    tiles.setTile(tileX, tileY, 2);
+                }
+            }
+        }
+        graphic = tiles;
     }
 }
 
--- source/scenes/GameScene.hx
+++ source/scenes/GameScene.hx
@@ -1,16 +1,192 @@
 package scenes;
 
-import haxepunk.*;
 import entities.*;
+import haxepunk.*;
+import haxepunk.input.*;
+import haxepunk.masks.*;
+import haxepunk.math.*;
+import openfl.Assets;
 
 class GameScene extends Scene
 {
+    public static inline var MAP_TILE_SIZE = 16;
+
+    private var roomMapBlueprint:Grid;
+    private var hallwayMapBlueprint:Grid;
+    private var shaftMapBlueprint:Grid;
+    private var allBlueprint:Grid;
+    private var map:Grid;
+    private var allLevels:Array<Level>;
+    private var player:Player;
+
     override public function begin() {
-        add(new Player(152, 82));
-        add(new Level("testlevel"));
+        loadMaps(0);
+        placeLevels();
+        player = new Player(152, 82);
+        add(player);
     }
 
     override public function update() {
         super.update();
+        camera.setTo(player.centerX, player.centerY, 0.5, 0.5);
+    }
+
+    private function loadMaps(mapNumber:Int) {
+        var mapPath = 'maps/${'test'}.oel';
+        var xml = Xml.parse(Assets.getText(mapPath));
+        var fastXml = new haxe.xml.Fast(xml.firstElement());
+        var mapWidth = Std.parseInt(fastXml.node.width.innerData);
+        var mapHeight = Std.parseInt(fastXml.node.height.innerData);
+        map = new Grid(mapWidth, mapHeight, MAP_TILE_SIZE, MAP_TILE_SIZE);
+        roomMapBlueprint = new Grid(
+            mapWidth, mapHeight, MAP_TILE_SIZE, MAP_TILE_SIZE
+        );
+        hallwayMapBlueprint = new Grid(
+            mapWidth, mapHeight, MAP_TILE_SIZE, MAP_TILE_SIZE
+        );
+        shaftMapBlueprint = new Grid(
+            mapWidth, mapHeight, MAP_TILE_SIZE, MAP_TILE_SIZE
+        );
+        allBlueprint = new Grid(
+            mapWidth, mapHeight, MAP_TILE_SIZE, MAP_TILE_SIZE
+        );
+        for (r in fastXml.node.rooms.nodes.rect) {
+            roomMapBlueprint.setRect(
+                Std.int(Std.parseInt(r.att.x) / MAP_TILE_SIZE),
+                Std.int(Std.parseInt(r.att.y) / MAP_TILE_SIZE),
+                Std.int(Std.parseInt(r.att.w) / MAP_TILE_SIZE),
+                Std.int(Std.parseInt(r.att.h) / MAP_TILE_SIZE)
+            );
+            allBlueprint.setRect(
+                Std.int(Std.parseInt(r.att.x) / MAP_TILE_SIZE),
+                Std.int(Std.parseInt(r.att.y) / MAP_TILE_SIZE),
+                Std.int(Std.parseInt(r.att.w) / MAP_TILE_SIZE),
+                Std.int(Std.parseInt(r.att.h) / MAP_TILE_SIZE)
+            );
+        }
+        for (r in fastXml.node.hallways.nodes.rect) {
+            hallwayMapBlueprint.setRect(
+                Std.int(Std.parseInt(r.att.x) / MAP_TILE_SIZE),
+                Std.int(Std.parseInt(r.att.y) / MAP_TILE_SIZE),
+                Std.int(Std.parseInt(r.att.w) / MAP_TILE_SIZE),
+                Std.int(Std.parseInt(r.att.h) / MAP_TILE_SIZE)
+            );
+            allBlueprint.setRect(
+                Std.int(Std.parseInt(r.att.x) / MAP_TILE_SIZE),
+                Std.int(Std.parseInt(r.att.y) / MAP_TILE_SIZE),
+                Std.int(Std.parseInt(r.att.w) / MAP_TILE_SIZE),
+                Std.int(Std.parseInt(r.att.h) / MAP_TILE_SIZE)
+            );
+        }
+        for (r in fastXml.node.shafts.nodes.rect) {
+            shaftMapBlueprint.setRect(
+                Std.int(Std.parseInt(r.att.x) / MAP_TILE_SIZE),
+                Std.int(Std.parseInt(r.att.y) / MAP_TILE_SIZE),
+                Std.int(Std.parseInt(r.att.w) / MAP_TILE_SIZE),
+                Std.int(Std.parseInt(r.att.h) / MAP_TILE_SIZE)
+            );
+            allBlueprint.setRect(
+                Std.int(Std.parseInt(r.att.x) / MAP_TILE_SIZE),
+                Std.int(Std.parseInt(r.att.y) / MAP_TILE_SIZE),
+                Std.int(Std.parseInt(r.att.w) / MAP_TILE_SIZE),
+                Std.int(Std.parseInt(r.att.h) / MAP_TILE_SIZE)
+            );
+        }
+    }
+
+    private function sealLevel(
+        level:Level, tileX:Int, tileY:Int, checkX:Int, checkY:Int
+    ) {
+        if(
+            !roomMapBlueprint.getTile(tileX + checkX - 1, tileY + checkY)
+            && !hallwayMapBlueprint.getTile(tileX + checkX - 1, tileY + checkY)
+        ) {
+            level.fillLeft(checkY);
+        }
+        if(
+            !roomMapBlueprint.getTile(tileX + checkX + 1, tileY + checkY)
+            && !hallwayMapBlueprint.getTile(tileX + checkX + 1, tileY + checkY)
+        ) {
+            level.fillRight(checkY);
+        }
+        if(
+            !roomMapBlueprint.getTile(tileX + checkX, tileY + checkY - 1)
+            && !shaftMapBlueprint.getTile(tileX + checkX, tileY + checkY - 1)
+        ) {
+            level.fillTop(checkX);
+        }
+        if(
+            !roomMapBlueprint.getTile(tileX + checkX, tileY + checkY + 1)
+            && !shaftMapBlueprint.getTile(tileX + checkX, tileY + checkY + 1)
+        ) {
+            level.fillBottom(checkX);
+        }
+    }
+
+    private function placeLevels() {
+        allLevels = new Array<Level>();
+        var levelTypes = ["room", "hallway", "shaft"];
+        var count = 0;
+        for(mapBlueprint in [
+            roomMapBlueprint, hallwayMapBlueprint, shaftMapBlueprint
+        ]) {
+            for(tileX in 0...mapBlueprint.columns) {
+                for(tileY in 0...mapBlueprint.rows) {
+                    if(
+                        mapBlueprint.getTile(tileX, tileY)
+                        && !map.getTile(tileX, tileY)
+                    ) {
+                        var canPlace = false;
+                        while(!canPlace) {
+                            var level = new Level(
+                                tileX * Level.MIN_LEVEL_WIDTH,
+                                tileY * Level.MIN_LEVEL_HEIGHT,
+                                levelTypes[count]
+                            );
+                            var levelWidth = Std.int(
+                                level.width / Level.MIN_LEVEL_WIDTH
+                            );
+                            var levelHeight = Std.int(
+                                level.height / Level.MIN_LEVEL_HEIGHT
+                            );
+                            canPlace = true;
+                            for(checkX in 0...levelWidth) {
+                                for(checkY in 0...levelHeight) {
+                                    if(
+                                        map.getTile(
+                                            tileX + checkX, tileY + checkY
+                                        )
+                                        || !mapBlueprint.getTile(
+                                            tileX + checkX, tileY + checkY
+                                        )
+                                    ) {
+                                        canPlace = false;
+                                    }
+                                }
+                            }
+                            if(canPlace) {
+                                for(checkX in 0...levelWidth) {
+                                    for(checkY in 0...levelHeight) {
+                                        map.setTile(
+                                            tileX + checkX, tileY + checkY
+                                        );
+                                        sealLevel(
+                                            level,
+                                            tileX, tileY,
+                                            checkX, checkY
+                                        );
+                                    }
+                                }
+                                level.addPathsUp();
+                                level.updateGraphic();
+                                add(level);
+                                allLevels.push(level);
+                            }
+                        }
+                    }
+                }
+            }
+            count++;
+        }
     }
 }
