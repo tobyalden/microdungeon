3917f61e7e64c20f22cadb70d1eb6069e79f5def
Toby Alden <toby.alden@gmail.com>
2019-12-16 23:19:14 -0800

    Add sawblade

---
+++ source/entities/Sawblade.hx
@@ -0,0 +1,38 @@
+package entities;
+
+import haxepunk.*;
+import haxepunk.graphics.*;
+import haxepunk.input.*;
+import haxepunk.masks.*;
+import haxepunk.math.*;
+import haxepunk.Tween;
+import haxepunk.tweens.motion.*;
+import haxepunk.tweens.misc.*;
+import haxepunk.utils.*;
+
+class Sawblade extends MiniEntity
+{
+    public static inline var CYCLE_TIME = 1;
+
+    private var sprite:Spritemap;
+    private var path:LinearMotion;
+
+    public function new(startX:Float, startY:Float, pathEnd:Vector2) {
+        super(startX, startY);
+        type = "hazard";
+        mask = new Circle(50);
+        sprite = new Spritemap("graphics/sawblade.png", 50, 50);
+        sprite.add("idle", [0]);
+        graphic = sprite;
+        path = new LinearMotion(TweenType.PingPong);
+        path.setMotion(
+            startX, startY, pathEnd.x, pathEnd.y, CYCLE_TIME, Ease.sineInOut
+        );
+        addTween(path, true);
+    }
+
+    override public function update() {
+        moveTo(path.x, path.y);
+        super.update();
+    }
+}
--- source/scenes/GameScene.hx
+++ source/scenes/GameScene.hx
@@ -1,12 +1,16 @@
 package scenes;
 
 import haxepunk.*;
+import haxepunk.math.*;
 import entities.*;
 
 class GameScene extends Scene
 {
     override public function begin() {
-        add(new Player(152, 82));
+        add(new Player(10, 50));
+        add(new Sawblade(
+            52, HXP.height - 25, new Vector2(252, HXP.height - 25)
+        ));
         add(new Level("testlevel"));
     }
 
